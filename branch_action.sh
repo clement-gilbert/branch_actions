6e479192ac8e7e15cd9d0ded66e80b97653f8793
clement <clement@qopius.com>
2020-01-25 19:08:05 +0100

    fix - Get the parent folder of the sh file to launch the python file relative to it

--- branch_action.sh
+++ branch_action.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+script_parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-source ./env/bin/activate
-python3 ./main.py
-
-
-
+source "$script_parent_path/env/bin/activate"
+python3 "$script_parent_path/main.py"
 
